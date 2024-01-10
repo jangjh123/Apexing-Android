bda775dd9c18de852f9d92589d41bf10af27e6a1
jangjh123 <gn0001231@gmail.com>
2024-01-10 20:23:38 +0900

    test: RequestBuilder 를 통해 Path 추가해서 Url 수정된 요청 생성하는 테스트 코드 작성

--- app/src/test/java/jyotti/apexing/apexing_android/RetrofitTest.kt
+++ app/src/test/java/jyotti/apexing/apexing_android/RetrofitTest.kt
@@ -2,10 +2,13 @@ package jyotti.apexing.apexing_android
 
 import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.shouldBe
+import okhttp3.Request
 
 class RetrofitTest : BehaviorSpec({
+    val testPath = "https://test.com/"
+
     Given("모든 요청에는 /.json Path 가 필요하다") {
-        val originalRequest = okhttp3.Request.Builder().url("https://test.com/").build()
+        val originalRequest = Request.Builder().url(testPath).build()
         val originalUrl = originalRequest.url
 
         When("OkHttp 를 통해 /.json Path 추가한다") {
@@ -14,6 +17,30 @@ class RetrofitTest : BehaviorSpec({
 
             Then("baseUrl 에 해당 Path 가 추가된다") {
                 newUrlBuilder.build().toUrl().toString() shouldBe "https://test.com/.json"
+            }
+        }
+    }
+
+    Given("특정 요청에는 /.json Path 가 추가된다") {
+        val path = ".path"
+        val requestList = arrayOf(
+            Pair(Request.Builder().url(testPath).build(), false),
+            Pair(Request.Builder().url(testPath).build(), false)
+        )
+
+        When("0번 인덱스의 요청에만 추가한다") {
+            requestList[0] = requestList[0].copy(second = true)
+
+            Then("조건을 통해 Path 를 추가한다") {
+                requestList.forEach { pair ->
+                    val request = pair.first
+                    val isPathNeeded = pair.second
+                    if (isPathNeeded) {
+                        request.newBuilder().url(request.url.toString() + path).build().url.toString() shouldBe "$testPath$path"
+                    } else {
+                        request.url.toString() shouldBe testPath
+                    }
+                }
             }
         }
     }
