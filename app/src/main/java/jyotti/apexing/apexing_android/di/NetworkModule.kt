33e701e8d559eead508d4758e8bc998af6f30368
jangjh123 <gn0001231@gmail.com>
2024-01-10 20:24:37 +0900

    feat: Url 수정하여 적용된 요청 생성하는 인터셉터 구현 및 적용

--- app/src/main/java/jyotti/apexing/apexing_android/di/NetworkModule.kt
+++ app/src/main/java/jyotti/apexing/apexing_android/di/NetworkModule.kt
@@ -4,15 +4,57 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import jyotti.apexing.apexing_android.BuildConfig.BASE_URL
+import jyotti.apexing.apexing_android.data.remote.ApiService
+import jyotti.apexing.apexing_android.data.remote.FirebaseRequestInterceptor
 import jyotti.apexing.apexing_android.data.remote.NetworkManager
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object NetworkModule {
+    private const val CONNECT_TIMEOUT_SEC = 5L
+    private const val READ_TIMEOUT_SEC = 10L
+    private const val WRITE_TIMEOUT_SEC = 5L
 
     @Provides
     @Singleton
     fun provideNetworkManager() = NetworkManager()
 
+    @Singleton
+    @Provides
+    fun provideFirebaseRequestInterceptor(): FirebaseRequestInterceptor = FirebaseRequestInterceptor()
+
+    @Singleton
+    @Provides
+    fun provideOkHttpClient(firebaseRequestInterceptor: FirebaseRequestInterceptor): OkHttpClient =
+        OkHttpClient.Builder()
+            .connectTimeout(CONNECT_TIMEOUT_SEC, TimeUnit.SECONDS)
+            .readTimeout(READ_TIMEOUT_SEC, TimeUnit.SECONDS)
+            .writeTimeout(WRITE_TIMEOUT_SEC, TimeUnit.SECONDS)
+            .addInterceptor(firebaseRequestInterceptor)
+            .build()
+
+    @Singleton
+    @Provides
+    fun provideGsonConverterFactory(): GsonConverterFactory = GsonConverterFactory.create()
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(
+        okHttpClient: OkHttpClient,
+        gsonConverterFactory: GsonConverterFactory
+    ): Retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .client(okHttpClient)
+        .addConverterFactory(gsonConverterFactory)
+        .build()
+
+    @Provides
+    @Singleton
+    fun provideApiService(retrofit: Retrofit): ApiService = retrofit.create(ApiService::class.java)
 }
