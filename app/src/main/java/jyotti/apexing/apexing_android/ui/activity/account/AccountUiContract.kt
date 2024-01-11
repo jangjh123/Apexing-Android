1b8006c2c4ce05406c53093d51617e93826ad64f
jangjh123 <gn0001231@gmail.com>
2024-01-11 15:13:21 +0900

    refactor: UiState, UiEffect 선언을BaseViewModel 에서 강제하도록 구현

---
+++ app/src/main/java/jyotti/apexing/apexing_android/ui/activity/account/AccountUiContract.kt
@@ -0,0 +1,14 @@
+package jyotti.apexing.apexing_android.ui.activity.account
+
+import jyotti.apexing.apexing_android.base.BaseContract
+
+interface AccountUiContract : BaseContract<AccountUiContract.UiState, AccountUiContract.UiEffect> {
+    data class UiState(
+        val isLoading: Boolean = false,
+        val idText: String = ""
+    )
+
+    sealed interface UiEffect {
+        object GoToHelpPage : UiEffect
+    }
+}
