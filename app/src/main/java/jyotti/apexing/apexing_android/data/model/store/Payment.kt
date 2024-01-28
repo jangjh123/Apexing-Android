076383eac3189e5a9be3b5458b6a58b5de983b09
jangjh123 <gn0001231@gmail.com>
2024-01-29 03:43:57 +0900

    remove: 사용하지 않는 클래스 삭제 처리

--- app/src/main/java/jyotti/apexing/apexing_android/data/model/store/Payment.kt
+++
@@ -1,6 +0,0 @@
-package jyotti.apexing.apexing_android.data.model.store
-
-data class Payment(
-    val ref: String,
-    val quantity: Int
-)
--- app/src/main/java/jyotti/apexing/apexing_android/data/model/store/StoreItem.kt
+++
@@ -1,9 +0,0 @@
-package jyotti.apexing.apexing_android.data.model.store
-
-data class StoreItem(
-    val title: String,
-    val expireTimeStamp: Long,
-    val shopTime: String,
-    val pricing: List<Payment>,
-    val asset: String
-)
