419602038219f79cfa4faa43bce8738de8c1e227
jangjh123 <gn0001231@gmail.com>
2024-01-30 16:07:41 +0900

    refactor: Fragment Show&Hide 구현

--- app/src/main/java/jyotti/apexing/apexing_android/ui/activity/home/HomeActivity.kt
+++ app/src/main/java/jyotti/apexing/apexing_android/ui/activity/home/HomeActivity.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.Intent
 import androidx.activity.viewModels
 import androidx.core.os.bundleOf
+import androidx.fragment.app.Fragment
 import com.google.android.material.tabs.TabLayout
 import dagger.hilt.android.AndroidEntryPoint
 import jyotti.apexing.apexing_android.base.BaseActivity
@@ -27,30 +28,70 @@ class HomeActivity : BaseActivity<ActivityHomeBinding>(ActivityHomeBinding::infl
     }
 
     private fun initTabLayout() {
-        binding.layoutTab.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
-            override fun onTabSelected(tab: TabLayout.Tab?) {
-                when (tab?.position) {
-                    0 -> showFragment(MAIN)
-                    1 -> showFragment(STATISTICS)
+        bind {
+            layoutTab.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+                override fun onTabSelected(tab: TabLayout.Tab?) {
+                    when (tab?.position) {
+                        0 -> showFragment(MAIN)
+                        1 -> showFragment(STATISTICS)
+                    }
                 }
-            }
 
-            override fun onTabUnselected(tab: TabLayout.Tab?) {}
-            override fun onTabReselected(tab: TabLayout.Tab?) {}
-        })
+                override fun onTabUnselected(tab: TabLayout.Tab?) {}
+                override fun onTabReselected(tab: TabLayout.Tab?) {}
+            })
+        }
     }
 
     private fun showFragment(apexingFragment: ApexingFragment) {
-        val fragmentClass = when (apexingFragment) {
-            MAIN -> MainFragment::class.java
-            STATISTICS -> StatisticsFragment::class.java
-        }
+        val fragment = getAddedFragment(apexingFragment)
 
-        supportFragmentManager
-            .beginTransaction()
-            .replace(binding.layoutFrame.id, fragmentClass, bundleOf(Pair(KEY_ID, intent.getStringExtra(KEY_ID))))
-            .addToBackStack(null)
-            .commit()
+        fragment?.let {
+            supportFragmentManager
+                .beginTransaction()
+                .show(fragment)
+                .commit()
+
+            hideOtherFragments(apexingFragment)
+        } ?: run {
+            val fragmentClass = when (apexingFragment) {
+                MAIN -> MainFragment::class.java
+                STATISTICS -> StatisticsFragment::class.java
+            }
+
+            supportFragmentManager
+                .beginTransaction()
+                .add(
+                    binding.layoutFrame.id,
+                    fragmentClass,
+                    bundleOf(Pair(KEY_ID, intent.getStringExtra(KEY_ID))),
+                    getFragmentTag(apexingFragment)
+                )
+                .addToBackStack(null)
+                .commit()
+        }
+    }
+
+    private fun getAddedFragment(apexingFragment: ApexingFragment): Fragment? {
+        return supportFragmentManager.findFragmentByTag(getFragmentTag(apexingFragment))
+    }
+
+    private fun getFragmentTag(apexingFragment: ApexingFragment): String = when (apexingFragment) {
+        MAIN -> TAG_MAIN_FRAGMENT
+        STATISTICS -> TAG_STATISTICS_FRAGMENT
+    }
+
+    private fun hideOtherFragments(exceptionalFragment: ApexingFragment) {
+        ApexingFragment.values().forEach { fragmentType ->
+            if (fragmentType != exceptionalFragment) {
+                getAddedFragment(fragmentType)?.let { fragment ->
+                    supportFragmentManager
+                        .beginTransaction()
+                        .hide(fragment)
+                        .commit()
+                }
+            }
+        }
     }
 
     override fun collectUiEffect() {
@@ -64,6 +105,8 @@ class HomeActivity : BaseActivity<ActivityHomeBinding>(ActivityHomeBinding::infl
     }
 
     companion object {
+        private const val TAG_MAIN_FRAGMENT = "tag_main_fragment"
+        private const val TAG_STATISTICS_FRAGMENT = "tag_statistics_fragment"
         const val KEY_ID = "key_id"
 
         fun newIntent(
